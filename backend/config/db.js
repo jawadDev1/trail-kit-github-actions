42e1edabd0af4304438ba9e4b94935b0672648c7
jawadDev1 <jawadali.dev1@gmail.com>
2026-06-19 09:41:18 +0500

    feat: github actions

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.DB_URL);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`MongoDB connection error: ${err.message}`);
